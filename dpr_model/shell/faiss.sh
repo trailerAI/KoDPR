7588d22c9f53c6611c6792135f9e50e7b0dda31c
Jisu-Kim <rlawltn9408@gmail.com>
2024-01-24 14:40:44 +0900

    Update faiss.sh

--- dpr_model/shell/faiss.sh
+++ dpr_model/shell/faiss.sh
@@ -1,4 +1,4 @@
-nohup python3 ./gen_db.py --config_path ./config/faiss_batch_32.yaml > ./logs/faiss_batch_32.log &
-nohup python3 ./gen_db.py --config_path ./config/faiss_batch_16.yaml > ./logs/faiss_batch_16.log &
-nohup python3 ./gen_db.py --config_path ./config/faiss_batch_4.yaml > ./logs/faiss_batch_4.log &
-nohup python3 ./gen_db.py --config_path ./config/faiss_batch_8.yaml > ./logs/faiss_batch_8.log &
+nohup python gen_db.py --config_path ./config/faiss_batch_32.yaml > ./logs/faiss_batch_32.log &
+nohup python gen_db.py --config_path ./config/faiss_batch_16.yaml > ./logs/faiss_batch_16.log &
+nohup python gen_db.py --config_path ./config/faiss_batch_4.yaml > ./logs/faiss_batch_4.log &
+nohup python gen_db.py --config_path ./config/faiss_batch_8.yaml > ./logs/faiss_batch_8.log &
