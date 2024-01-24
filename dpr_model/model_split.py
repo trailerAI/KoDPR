cddf09c5abac837d15fe0ad5e20e3116fbc8ad83
Jisu-Kim <rlawltn9408@gmail.com>
2024-01-24 15:05:43 +0900

    Update model_split.py

--- dpr_model/model_split.py
+++ dpr_model/model_split.py
@@ -1,37 +1,35 @@
-import json
 import torch
+import argparse
 
-# 모델 파일 경로
-# model_path = '/home/jisukim/DPR/dpr_model/results/best_model_gold32_3.pt'
-# model_path = '/ml_data/retrieval_model/results/best_model_gold16_3.pt'
-# model_path = '/ml_data/retrieval_model/results/best_model_gold8_5.pt'
-model_path = '/ml_data/retrieval_model/results/best_model_gold4_5.pt'
+def main(model_path, question_path, passage_path):
+    model = torch.load(model_path, map_location=torch.device('cpu'))
 
-# 모델 로드
-model = torch.load(model_path, map_location=torch.device('cpu'))
+    from collections import OrderedDict
 
-from collections import OrderedDict
+    ordered_dict_question = OrderedDict()
+    ordered_dict_passage = OrderedDict()
 
-ordered_dict_question = OrderedDict()
-ordered_dict_passage = OrderedDict()
+    for key, value in model.items():
+        if key.split('.')[0] == "question_encoder":
+            key = '.'.join(key.split('.')[1:])
+            ordered_dict_question[key] = value
+        if key.split('.')[0] == "passage_encoder":
+            key = '.'.join(key.split('.')[1:])
+            ordered_dict_passage[key] = value
 
-for key, value in model.items():
-    if key.split('.')[0] == "question_encoder":
-        ## load하기 위해 encoder로 rename
-        key = '.'.join(key.split('.')[1:])
-        ordered_dict_question[key] = value
-    if key.split('.')[0] == "passage_encoder":
-        key = '.'.join(key.split('.')[1:])
-        ordered_dict_passage[key] = value
+    torch.save(ordered_dict_question, question_path)
+    torch.save(ordered_dict_passage, passage_path)
 
-# torch.save(ordered_dict_question, "/home/jisukim/DPR/dpr_model/results/question_gold_32.pt")
-# torch.save(ordered_dict_passage, "/home/jisukim/DPR/dpr_model/results/passage_gold_32.pt")
 
-# torch.save(ordered_dict_question, "/home/jisukim/DPR/dpr_model/results/question_gold_16.pt")
-# torch.save(ordered_dict_passage, "/home/jisukim/DPR/dpr_model/results/passage_gold_16.pt")
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run script")
+    parser.add_argument('-model_path', '--model_path', type=str, help='DPR model path', required=True)
+    parser.add_argument('-question_path', '--question_path', type=str, help='Question model path', required=True)
+    parser.add_argument('-passage_path', '--passage_path', type=str, help='Passage model path', required=True)
 
-# torch.save(ordered_dict_question, "/home/jisukim/DPR/dpr_model/results/question_gold_8.pt")
-# torch.save(ordered_dict_passage, "/home/jisukim/DPR/dpr_model/results/passage_gold_8.pt")
+    args = parser.parse_args()
+    return args.model_path, args.question_path, args.passage_path
 
-torch.save(ordered_dict_question, "/home/jisukim/DPR/dpr_model/results/question_gold_4.pt")
-torch.save(ordered_dict_passage, "/home/jisukim/DPR/dpr_model/results/passage_gold_4.pt")
+
+if __name__ == '__main__':
+    main(*parse_args())
