8e38c9d75947986b7eb946a819f44b04201b3711
Jisu-Kim <rlawltn9408@gmail.com>
2024-01-24 14:38:30 +0900

    Update faiss.sh

--- dpr_model/shell/faiss.sh
+++ dpr_model/shell/faiss.sh
@@ -1,4 +1,4 @@
-# nohup python3 /home/jisukim/DPR/dpr_model/gen_db.py --config_path /home/jisukim/DPR/dpr_model/config/faiss_batch_32.yaml > /home/jisukim/DPR/dpr_model/logs/faiss_batch_32.log &
-# nohup python3 /home/jisukim/DPR/dpr_model/gen_db.py --config_path /home/jisukim/DPR/dpr_model/config/faiss_batch_16.yaml > /home/jisukim/DPR/dpr_model/logs/faiss_batch_16.log &
+nohup python3 /home/jisukim/DPR/dpr_model/gen_db.py --config_path /home/jisukim/DPR/dpr_model/config/faiss_batch_32.yaml > /home/jisukim/DPR/dpr_model/logs/faiss_batch_32.log &
+nohup python3 /home/jisukim/DPR/dpr_model/gen_db.py --config_path /home/jisukim/DPR/dpr_model/config/faiss_batch_16.yaml > /home/jisukim/DPR/dpr_model/logs/faiss_batch_16.log &
 nohup python3 /home/jisukim/DPR/dpr_model/gen_db.py --config_path /home/jisukim/DPR/dpr_model/config/faiss_batch_4.yaml > /home/jisukim/DPR/dpr_model/logs/faiss_batch_4.log &
 nohup python3 /home/jisukim/DPR/dpr_model/gen_db.py --config_path /home/jisukim/DPR/dpr_model/config/faiss_batch_8.yaml > /home/jisukim/DPR/dpr_model/logs/faiss_batch_8.log &
