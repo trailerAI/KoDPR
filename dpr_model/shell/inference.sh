e3795ad6dc8493fdbb6b8314fc111345798593cd
Jisu-Kim <rlawltn9408@gmail.com>
2024-01-24 14:40:58 +0900

    Update inference.sh

--- dpr_model/shell/inference.sh
+++ dpr_model/shell/inference.sh
@@ -1,4 +1,4 @@
-nohup python3 inference.py --config_path ./config/inference_batch_32_test.yaml > ./logs/inference_batch_32_test.log &
-nohup python3 inference.py --config_path ./config/inference_batch_16_test.yaml > ./logs/inference_batch_16_test.log &
-nohup python3 inference.py --config_path ./config/inference_batch_8_test.yaml > ./logs/inference_batch_8_test.log &
-nohup python3 inference.py --config_path ./config/inference_batch_4_test.yaml > ./logs/inference_batch_4_test.log &
+nohup python inference.py --config_path ./config/inference_batch_32_test.yaml > ./logs/inference_batch_32_test.log &
+nohup python inference.py --config_path ./config/inference_batch_16_test.yaml > ./logs/inference_batch_16_test.log &
+nohup python inference.py --config_path ./config/inference_batch_8_test.yaml > ./logs/inference_batch_8_test.log &
+nohup python inference.py --config_path ./config/inference_batch_4_test.yaml > ./logs/inference_batch_4_test.log &
