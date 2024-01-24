010097fea3a8ccd912ba5490579f5f8ed8ff915d
Jisu-Kim <rlawltn9408@gmail.com>
2024-01-24 14:40:27 +0900

    Update inference.sh

--- dpr_model/shell/inference.sh
+++ dpr_model/shell/inference.sh
@@ -1,6 +1,4 @@
-# nohup python3 /home/jisukim/DPR/dpr_model/inference.py --config_path /home/jisukim/DPR/dpr_model/config/inference_batch_32_test.yaml > /home/jisukim/DPR/dpr_model/logs/inference_batch_32.log &
-# nohup python3 /home/jisukim/DPR/dpr_model/inference.py --config_path /home/jisukim/DPR/dpr_model/config/inference_batch_32_train1.yaml > /home/jisukim/DPR/dpr_model/logs/inference_batch_32_train1.log &
-## 아래부터 돌려야함
-nohup python3 /home/jisukim/DPR/dpr_model/inference.py --config_path /home/jisukim/DPR/dpr_model/config/inference_batch_32_train2.yaml > /home/jisukim/DPR/dpr_model/logs/inference_batch_32_train2.log &
-nohup python3 /home/jisukim/DPR/dpr_model/inference.py --config_path /home/jisukim/DPR/dpr_model/config/inference_batch_32_train3.yaml > /home/jisukim/DPR/dpr_model/logs/inference_batch_32_train3.log &
-nohup python3 /home/jisukim/DPR/dpr_model/inference.py --config_path /home/jisukim/DPR/dpr_model/config/inference_batch_32_valid.yaml > /home/jisukim/DPR/dpr_model/logs/inference_batch_32_valid.log &
+nohup python3 inference.py --config_path ./config/inference_batch_32_test.yaml > ./logs/inference_batch_32_test.log &
+nohup python3 inference.py --config_path ./config/inference_batch_16_test.yaml > ./logs/inference_batch_16_test.log &
+nohup python3 inference.py --config_path ./config/inference_batch_8_test.yaml > ./logs/inference_batch_8_test.log &
+nohup python3 inference.py --config_path ./config/inference_batch_4_test.yaml > ./logs/inference_batch_4_test.log &
